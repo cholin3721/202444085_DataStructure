f31b7953bb00e7ebf774f49e101f5f859c9a6556
cholin3721 <cholin3721@gmail.com>
2025-03-17 15:06:56 +0900

    v0.2 O(1), O(n) comparison

--- week02/week02.py
+++ week02/week02.py
@@ -1,6 +1,9 @@
 # alt + shift + f10
 n = int(input("Input the Integer : "))
-result = 0
-for i in range(n+1) :
-    result = result + i
+# result = 0
+# for i in range(n+1) :
+#     result = result + i  # O(n) 선형 시간을 가짐
+result = n * (n+1) // 2  # O(1) 상수 시간을 가짐
 print(result)
+
+
