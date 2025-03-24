368cc3b0bea3569e3e59612bc0099f0e20227e5f
NastyCRacker <cholin3721@gmail.com>
2025-03-24 15:15:32 +0900

    v intersection

--- week03/week03.py
+++ week03/week03.py
@@ -1,22 +1,36 @@
-def duplicate_city(cities) :
-    result_city = []
-    s = set()
-    for city in cities :
-        l1 = len(s)
-        s.add(city)
-        l2 = len(s)
-        if l1 == l2 :
-            result_city.append(city)
-    return result_city
+def inters(l1, l2) :
+    l3 = list()
+    for v in l1 :
+        if v in l2 :
+            l3.append(v)
+    return l3
 
-cities = ['Suwon', 'Hwasung', 'Incheon', 'Bucheon', 'Incheon', 'Seoul']
-# cities = set(cities)
-cities.append('Seoul')
-cities.append('Anyang')
-cities.append('Incheon')
+l1 = [45, 5, 22, 31, 7, 19]
+l2 = [2, 1, 5, 22, 7, 38, 27, 19, 13, 41]
 
-print(cities)
-print(set(duplicate_city(cities)))
+print(inters(l1, l2))
+
+
+
+# def duplicate_city(cities) :
+#     result_city = []
+#     s = set()
+#     for city in cities :
+#         l1 = len(s)
+#         s.add(city)
+#         l2 = len(s)
+#         if l1 == l2 :
+#             result_city.append(city)
+#     return result_city
+#
+# cities = ['Suwon', 'Hwasung', 'Incheon', 'Bucheon', 'Incheon', 'Seoul']
+# # cities = set(cities)
+# cities.append('Seoul')
+# cities.append('Anyang')
+# cities.append('Incheon')
+#
+# print(cities)
+# print(set(duplicate_city(cities)))
 
 # groups = ['HOT', 'Seventeen', 'Black Pink', 'NJZ']
 #
