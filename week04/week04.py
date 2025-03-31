2f0f3eecae8ff77e2d8be4c647f87f615d9750d3
NastyCRacker <cholin3721@gmail.com>
2025-03-31 15:07:21 +0900

    Linked List Random Number Search

--- week04/week04.py
+++ week04/week04.py
@@ -1,3 +1,6 @@
+import random
+
+
 class Node:
     def __init__(self, data, link = None):
         self.data = data
@@ -59,13 +62,22 @@ class LinkedList :
         return f"{target} 은(는) 링크드 리스트 안에 존재하지 않습니다!"
 
 
-ll = LinkedList()
-ll.append(8)
-ll.append(10)
-ll.append(-9)
+# ll = LinkedList()
+# ll.append(8)
+# ll.append(10)
+# ll.append(-9)
+#
+# ll.print()
+# print()
+# print(ll)
+# print()
+# print(ll.search(10))  # __str__ method search method
 
-ll.print()
-print()
+ll = LinkedList()
+for _ in range(20) :
+    # j = random.randint(1, 30)
+    # ll.append(j)
+    # print(j, end=" ")  책에 있는 코드,
+    ll.append(random.randint(1, 30))
 print(ll)
-print()
 print(ll.search(10))
