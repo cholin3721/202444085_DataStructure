9f272f4074a0db7cd7573f7ba161f0f91936fe39
NastyCRacker <cholin3721@gmail.com>
2025-05-19 14:41:46 +0900

    dfs

---
+++ week11/graph_dfs.py
@@ -0,0 +1,22 @@
+graph = [
+    [0, 1, 1, 0, 0, 0, 0, 0],
+    [1, 0, 0, 1, 0, 0, 0, 0],
+    [1, 0, 0, 1, 0, 0, 0, 0],
+    [0, 1, 1, 0, 1, 1, 1, 0],
+    [0, 0, 0, 1, 0, 1, 0, 0],
+    [0, 0, 0, 1, 1, 0, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0, 1],
+    [0, 0, 0, 0, 0, 0, 1, 0]
+]
+
+def dfs(g, i, visited) :
+    visited[i] = 1
+    print(chr(ord('A') + i), end = ' ')
+    for j in range(len(g[i])) :
+        if g[i][j] == 1 and not visited[j]:
+            dfs(g, j, visited)
+
+
+visited_dfs = [0 for _ in range(len(graph))]
+
+dfs(graph, 6, visited_dfs)
