141646be67af0d0cfee090d047dd5a31f535c562
NastyCRacker <cholin3721@gmail.com>
2025-06-09 14:13:27 +0900

    baekjoon PrimeNumber

---
+++ week14/PrimeNumber.py
@@ -0,0 +1,22 @@
+import sys
+import math
+
+def is_prime(n) -> bool :
+    if n <= 1:
+        return False
+    elif n == 2:
+        return True
+    elif n % 2 == 0:
+        return False
+    else :
+        for n in range(3, int(math.sqrt(n)), 2):
+            if n % i == 0:
+                return False
+    return True
+
+
+m, n = map(int, sys.stdin.readline().rstrip().split())
+if m <= n :
+    for i in range(m, n+1) :
+        if is_prime(i) :
+            print(i)
